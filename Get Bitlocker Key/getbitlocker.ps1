9559f796d49bb49950d443b049d63c9d98d3589f
hyrexhun <arpad15b@gmail.com>
2024-07-20 15:47:29 +0200

    third cows

---
+++ Get Bitlocker Key/getbitlocker.ps1
@@ -0,0 +1,11 @@
+﻿Set-ExecutionPolicy Bypass -Scope Process -Force
+
+$LogLocate= "\\save\file\location\$env:COMPUTERNAME.txt"
+
+New-Item -Path "$LogLocate"
+$BitlockerVolumers = Get-BitLockerVolume
+$BitlockerVolumers |
+ForEach-Object {
+    $MountPoint = $_.MountPoint
+    $MountPoint,$BitlockerVolumers.KeyProtector > $LogLocate
+} 
